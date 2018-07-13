41cbebfc13ca1766f3b321f6f84fbb0fc55b68b0
hendychua <hendy@srcclr.com>
2018-07-13 11:34:26 +0800

    Change to multi-project solution.

--- example-dotnet/Program.cs
+++ example-dotnet/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 
+<<<<<<< HEAD
 using example_dotnet_utils;
 
+=======
+>>>>>>> 08920f3... Change to multi-project solution.
 namespace example_dotnet
 {
     class Program
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Say.hello("example");
+=======
+>>>>>>> 08920f3... Change to multi-project solution.
             Console.WriteLine("Hello World!");
         }
     }
